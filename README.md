033b612e7e1b4b97ea6f24398bc9ced853e5d9ae
Freewind <nowindlee@gmail.com>
2022-11-26 14:28:14 +0800

    typescript-vite-tippyjs-singleton-demo

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-TypeScript Vite Tippyjs Demo
+TypeScript Vite Tippyjs Singleton Demo
 ===========================
+
+每次只允许出现一个tooltip
+
+注意：每个tippy的instance本来都可以有各自的配置，但是一旦被createSingleton包起来之后，它们就必须
+使用singleton中的配置。只有在`overrides`中定义配置名允许被单个instance覆盖。
 
 ```
 npm install
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,23 @@
 import './index.css';
-import tippy from 'tippy.js';
+import tippy, {createSingleton} from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
-const button = document.querySelector("#main button");
-tippy(button!, {
-    content: 'Hello, tippy'
+// const buttons = document.querySelectorAll("#main button");
+
+const singleton = createSingleton([
+    ...tippy('#button1', {
+        content: '<button>Hello, tippy1</button>',
+        placement: 'right',
+        allowHTML:true
+    }),
+    ...tippy('#button2', {
+        content: '<button>Hello, tippy2</button>',
+        placement: 'right',
+    }),
+], {
+    delay: 500,
+    allowHTML: false,
+    // 只有出现在overrides中的属性，前面独立instance中的定义才能覆盖这里的通用定义
+    overrides: ['allowHTML']
 })
+singleton.show()
